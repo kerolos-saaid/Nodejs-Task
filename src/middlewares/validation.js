0de550bb268d375c6be18dc147d58947b6c963b2
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 22:02:26 +0300

    add validation middleware

---
+++ src/middlewares/validation.js
@@ -0,0 +1,36 @@
+import joi from "joi";
+const dataMethods = ["body", "params", "query", "headers", "file", "files"];
+
+export const generalFields = {
+  email: joi
+    .string()
+    .email({
+      minDomainSegments: 2,
+      maxDomainSegments: 4,
+      // tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  password: joi.string().min(8).max(30),
+  name: joi.string().min(3).max(30),
+};
+
+export const validation = (schema) => {
+  return (req, res, next) => {
+    const validationError = [];
+    dataMethods.forEach((key) => {
+      if (schema[key]) {
+        const validationResult = schema[key].validate(req[key], {
+          abortEarly: false,
+        });
+        if (validationResult.error) {
+          validationError.push(validationResult.error.details);
+        }
+      }
+    });
+
+    if (validationError.length) {
+      return res.json({ message: "Validation Error", validationError });
+    }
+    return next();
+  };
+};
