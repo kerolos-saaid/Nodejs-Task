4461b2f7e543c9aeb0176c169832d7333196845d
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 22:33:21 +0300

    edit envVariables.js file format

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -1,8 +1,11 @@
-export default {
+const envVariables = {
   PORT: process.env.PORT || 3000,
   TokenConfig: {
     AUTH_SIGNATURE: process.env.TOKEN_SECRET || "your-secret-key",
     AUTH_EXPIRE_TIME: process.env.TOKEN_EXPIRES_IN || "1h",
   },
   DATABASE_URL: process.env.DATABASE_URL,
+  HASH_SALT: process.env.HASH_SALT || 10,
 };
+
+export default envVariables;
