0a9c59c286959095d81c88611502bb7aa3fab2c6
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 19:01:54 +0300

    added globalErrorHandler, express.json and indexRouter

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import express from "express";
+import indexRouter from "./index.router.js";
+import { globalErrorHandler } from "./utils/ErrorHandler.js";
+
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use(indexRouter);
+app.use(globalErrorHandler);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
