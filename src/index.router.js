54671dd9c0cf8b6bf65c74ec3c335ac5cc2ccf3c
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 19:44:41 +0300

    add auth route

--- src/index.router.js
+++ src/index.router.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import userRoute from "./modules/User/user.route.js";
+import authRoute from "./modules/auth/auth.route.js";
 
 const router = Router();
 
+router.use("/auth", authRoute);
 router.use("/users", userRoute);
 
 export default router;
