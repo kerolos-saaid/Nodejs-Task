cf4143dbeb73d9d06ef0f57fcc87ce6010d70dcc
Kerolos <kerolos.saaid@gmail.com>
2025-05-13 02:44:58 +0300

    add post validation

--- src/modules/Post/post.route.js
+++ src/modules/Post/post.route.js
@@ -3,17 +3,22 @@ import auth from "../../middlewares/auth.js";
 import hasPermission from "../../middlewares/hasPermission.js";
 import PERMISSIONS from "../Auth/AccessControl/permissions.js";
 import * as postController from "./post.controller.js";
+import { validation } from "../../middlewares/validation.js";
+import * as postValidation from "./post.validation.js";
 
 const router = Router();
 
 router.post(
   "/create",
+  validation(postValidation.createPost),
   auth(),
   hasPermission([PERMISSIONS.CREATE_POST]),
   postController.createPost
 );
+
 router.delete(
   "/delete/:postId",
+  validation(postValidation.deletePost),
   auth(),
   hasPermission([PERMISSIONS.DELETE_OWN_POST]),
   postController.deletePost
