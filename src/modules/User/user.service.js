ddf00a30bd7fe41f362b087e7f9e94f6384ed3b1
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 21:19:56 +0300

    add role and permissions in createUser method

--- src/modules/User/user.service.js
+++ src/modules/User/user.service.js
@@ -4,13 +4,15 @@ import envVariables from "../../config/envVariables.js";
 import ROLES_PERMISSIONS from "../Auth/AccessControl/rolesPermissions.js";
 import ROLES from "../Auth/AccessControl/roles.js";
 
-export const createUser = async (userData, role) => {
-  const { email, password, name } = userData;
+export const createUser = async (userData) => {
+  const { email, password, name, role } = userData;
   const user = await prisma.user.create({
     data: {
       name,
       email,
       password: await bcrypt.hash(password, envVariables.HASH_SALT),
+      role,
+      permissions: ROLES_PERMISSIONS[role] || ROLES_PERMISSIONS[ROLES.USER],
     },
   });
   const { password: _, ...userWithoutPassword } = user;
