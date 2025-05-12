7b081ccbce3cb5bb0d08ad9de16969887b7cc72b
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 23:31:06 +0300

    add posts route

--- src/index.router.js
+++ src/index.router.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import userRoute from "./modules/User/user.route.js";
 import authRoute from "./modules/auth/auth.route.js";
+import postRoute from "./modules/Post/post.route.js";
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/users", userRoute);
+router.use("/posts", postRoute);
 
 export default router;
