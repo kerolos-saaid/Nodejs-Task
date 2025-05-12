31e3e963e9bebefddc78ffdc8518030ef4ecdcbd
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 22:00:19 +0300

    add auth validation

--- src/modules/Auth/auth.validation.js
+++ src/modules/Auth/auth.validation.js
@@ -2,11 +2,20 @@ import joi from "joi";
 import { generalFields } from "../../middlewares/validation.js";
 
 export const signup = {
-  body: joi.object().required().keys({
-    name: generalFields.name.required(),
-    email: generalFields.email.required(),
-    password: generalFields.password.required(),
-  }),
+  body: joi
+    .object()
+    .required()
+    .keys({
+      name: generalFields.name.required(),
+      email: generalFields.email.required(),
+      password: generalFields.password.required(),
+      role: joi
+        .string()
+        .valid("admin", "user")
+        .insensitive()
+        .uppercase()
+        .optional(),
+    }),
 };
 
 export const login = {
