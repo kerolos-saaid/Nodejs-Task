0c5a59c84fb078a0596616002096cba60fd80663
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 18:45:15 +0300

    removed promises from token utils and async/await from services

--- src/utils/Token.js
+++ src/utils/Token.js
@@ -1,27 +1,29 @@
 import jwt from "jsonwebtoken";
-import ApiError from "./ApiError";
+import ApiError from "./ApiError.js";
 import { StatusCodes } from "http-status-codes";
 
 export const createToken = (obj = {}, signature, expire_time) => {
-  return jwt.sign(obj, signature, { expiresIn: expire_time }, (err, token) => {
-    if (err) {
-      throw new ApiError(
-        "Error generating authentication token",
-        StatusCodes.INTERNAL_SERVER_ERROR
-      );
-    }
-    return token;
-  });
+  try {
+    return jwt.sign(obj, signature, {
+      expiresIn: expire_time,
+    });
+  } catch (error) {
+    throw new ApiError(
+      StatusCodes.INTERNAL_SERVER_ERROR,
+      "Token creation failed",
+      error
+    );
+  }
 };
 
 export const decodeToken = (token, signature) => {
-  return jwt.verify(token, signature, (err, decoded) => {
-    if (err) {
-      throw new ApiError(
-        "Invalid or expired authentication token",
-        StatusCodes.UNAUTHORIZED
-      );
-    }
-    return decoded;
-  });
+  try {
+    return jwt.verify(token, signature);
+  } catch (error) {
+    throw new ApiError(
+      StatusCodes.UNAUTHORIZED,
+      "Token verification failed",
+      error
+    );
+  }
 };
