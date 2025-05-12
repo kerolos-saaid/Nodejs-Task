78517c06edb96eeb9040e0f7acbd0bdfa15032a1
Kerolos <kerolos.saaid@gmail.com>
2025-05-12 22:32:24 +0300

    add hasPermission middleware

---
+++ src/middlewares/hasPermission.js
@@ -0,0 +1,18 @@
+import { StatusCodes } from "http-status-codes";
+import AsyncHandler from "../utils/AsyncHandler.js";
+
+const hasPermission = (permissions) => {
+  return AsyncHandler(async (req, res, next) => {
+    const hasPermission = permissions.some((permission) =>
+      user.permissions.includes(permission)
+    );
+    if (!hasPermission) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: "You do not have permission to perform this action",
+      });
+    }
+    next();
+  });
+};
+
+export default hasPermission;
