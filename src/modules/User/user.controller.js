961d518d041eb2598891b272168933549d31c3d6
Kerolos <kerolos.saaid@gmail.com>
2025-05-13 11:03:11 +0300

    add grantPermission and revokePermission in user controller

---
+++ src/modules/User/user.controller.js
@@ -0,0 +1,21 @@
+import { StatusCodes } from "http-status-codes";
+import AsyncHandler from "../../utils/AsyncHandler.js"; // Added .js extension
+import * as userService from "./user.service.js"; // Added import for userService
+
+export const grantPermissions = AsyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  const { permissions } = req.body;
+  const user = await userService.givePermissions(userId, permissions);
+  res
+    .status(StatusCodes.OK)
+    .json({ message: "Permissions granted successfully", user });
+});
+
+export const revokedPermissions = AsyncHandler(async (req, res) => {
+  const { userId } = req.params;
+  const { permissions } = req.body;
+  const user = await userService.revokePermissions(userId, permissions); // Assuming takePermissions service exists
+  res
+    .status(StatusCodes.OK)
+    .json({ message: "Permissions revoked successfully", user });
+});
