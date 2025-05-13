6f21123b8a62030a70737d7332983c5a356f6c8b
Kerolos <kerolos.saaid@gmail.com>
2025-05-13 11:04:29 +0300

    add routes for user permission management

--- src/modules/User/user.route.js
+++ src/modules/User/user.route.js
@@ -1,5 +1,24 @@
 import { Router } from "express";
+import * as userController from "./user.controller.js";
+import auth from "../../middlewares/auth.js";
+import ROLES from "../Auth/AccessControl/roles.js";
+import { validation } from "../../middlewares/validation.js";
+import * as userValidation from "./user.validation.js";
 
 const router = Router();
+
+router.put(
+  "/grant_permissions/:userId",
+  validation(userValidation.updateUserPermissionsSchema),
+  auth(ROLES.ADMIN),
+  userController.grantPermissions
+);
+
+router.put(
+  "/revoke_permissions/:userId",
+  validation(userValidation.updateUserPermissionsSchema),
+  auth(ROLES.ADMIN),
+  userController.revokedPermissions
+);
 
 export default router;
