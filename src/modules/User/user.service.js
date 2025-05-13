b4a1f95a83fd30f76a7d12b0b1725c89898503aa
Kerolos <kerolos.saaid@gmail.com>
2025-05-13 11:05:56 +0300

    add grant and revoke permissions methods

--- src/modules/User/user.service.js
+++ src/modules/User/user.service.js
@@ -3,6 +3,8 @@ import bcrypt from "bcrypt";
 import envVariables from "../../config/envVariables.js";
 import ROLES_PERMISSIONS from "../Auth/AccessControl/rolesPermissions.js";
 import ROLES from "../Auth/AccessControl/roles.js";
+import { StatusCodes } from "http-status-codes";
+import ApiError from "../../utils/ApiError.js";
 
 export const createUser = async (userData) => {
   const { email, password, name, role } = userData;
@@ -30,4 +32,48 @@ export const getUserByEmail = async (email) => {
   }
   const { password: _, ...userWithoutPassword } = user;
   return userWithoutPassword;
+};
+
+export const grantPermissions = async (userId, permissionsToGive) => {
+  const user = await prisma.user.findUnique({
+    where: { id: Number(userId) },
+  });
+
+  if (!user) {
+    throw new ApiError("User not found", StatusCodes.NOT_FOUND);
+  }
+
+  const updatedPermissions = [
+    ...new Set([...user.permissions, ...permissionsToGive]),
+  ];
+
+  const updatedUser = await prisma.user.update({
+    where: { id: Number(userId) },
+    data: { permissions: updatedPermissions },
+  });
+
+  const { password: _, ...userWithoutPasswordUpdated } = updatedUser;
+  return userWithoutPasswordUpdated;
+};
+
+export const revokePermissions = async (userId, permissionsToTake) => {
+  const user = await prisma.user.findUnique({
+    where: { id: Number(userId) },
+  });
+
+  if (!user) {
+    throw new ApiError("User not found", StatusCodes.NOT_FOUND);
+  }
+
+  const updatedPermissions = user.permissions.filter(
+    (p) => !permissionsToTake.includes(p)
+  );
+
+  const updatedUser = await prisma.user.update({
+    where: { id: Number(userId) },
+    data: { permissions: updatedPermissions },
+  });
+
+  const { password: _, ...userWithoutPasswordUpdated } = updatedUser;
+  return userWithoutPasswordUpdated;
 };
