82127702223909ea1ec6e56dc38402ee250cff22
Kerolos <kerolos.saaid@gmail.com>
2025-06-10 18:33:24 +0300

    add firebase environment variables config

--- src/config/envVariables.js
+++ src/config/envVariables.js
@@ -6,6 +6,15 @@ const envVariables = {
   },
   DATABASE_URL: process.env.DATABASE_URL,
   HASH_SALT: process.env.HASH_SALT || 10,
+  FIREBASE: {
+    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASURMENT_ID: process.env.FIREBASE_MEASURMENT_ID,
+  },
 };
 
 export default envVariables;
