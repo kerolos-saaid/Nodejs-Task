1ac21245b14ad16f4efd08b495ff94c84f92d55f
Kerolos <kerolos.saaid@gmail.com>
2025-06-10 19:11:58 +0300

    add sendPushNotification method

--- src/modules/shared/services/pushNotification.service.js
+++ src/modules/shared/services/pushNotification.service.js
@@ -1,9 +1,25 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
-import envVariables from "../../../config/envVariables";
+import envVariables from "../config/envVariables";
 
 // Initialize Firebase
 const app = initializeApp(envVariables.FIREBASE);
 
 // Initialize Firebase Cloud Messaging and get a reference to the service
 const messaging = getMessaging(app);
+
+export const sendPushNotification = async (token, notificationData) => {
+  try {
+    const response = await messaging.send({
+      token,
+      notification: {
+        title: notificationData.title,
+        body: notificationData.body,
+      },
+    });
+    return response;
+  } catch (error) {
+    console.error("Error sending push notification:", error);
+    throw new Error("Failed to send push notification");
+  }
+};
