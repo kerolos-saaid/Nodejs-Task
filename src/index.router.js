52ac50cc10d528a209be1c43bb786dd1f43b7519
Kerolos <kerolos.saaid@gmail.com>
2025-06-25 15:08:34 +0300

    Add notification route to index.router.js

--- src/index.router.js
+++ src/index.router.js
@@ -3,11 +3,13 @@ import { Router } from "express";
 import userRoute from "./modules/User/user.route.js";
 import authRoute from "./modules/auth/auth.route.js";
 import postRoute from "./modules/Post/post.route.js";
+import notificatioRoute from "./modules/Notification/notification.route.js";
 
 const router = Router();
 
 router.use("/auth", authRoute);
 router.use("/users", userRoute);
 router.use("/posts", postRoute);
+router.use("/notifications", notificatioRoute);
 
 export default router;
